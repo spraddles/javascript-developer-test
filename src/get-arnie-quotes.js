678a2a3ff4a4453ddff47a33aed5b463ac84cc66
Brett Spradbury <brett.spradbury@gmail.com>
2025-08-27 15:53:06 +1000

    add catch

--- src/get-arnie-quotes.js
+++ src/get-arnie-quotes.js
@@ -3,18 +3,23 @@ const { httpGet } = require('./mock-http-interface');
 const getArnieQuotes = async (urls) => {
   const results = await Promise.all(
     urls.map(async (url) => {
-      const response = await httpGet(url);
-      const { message } = JSON.parse(response.body);
-      if (response.status === 200) {
-        return { 'Arnie Quote': message };
-      } else {
-        return { 'FAILURE': message };
+      try {
+        const response = await httpGet(url)
+        const { message } = JSON.parse(response.body)
+
+        if (response.status === 200) {
+          return { 'Arnie Quote': message }
+        } else {
+          return { 'FAILURE': message }
+        }
+      } catch (err) {
+        return { 'FAILURE': message }
       }
     })
   )
 
-  return results;
-};
+  return results
+}
 
 module.exports = {
     getArnieQuotes,
